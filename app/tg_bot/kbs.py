2b18f7343f40b529a67937b728887cef0d47d8f2
Max Solovov <solovov@yandexlyceum.ru>
2025-03-10 16:13:43 +0300

    Time

--- app/tg_bot/kbs.py
+++ app/tg_bot/kbs.py
@@ -4,19 +4,19 @@ from app.config import settings
 
 main_kb = [
     [{"text": "📅 Мои записи", "callback_data": "booking"}],
-    [{"text": "🔖 Записаться", "web_app": {"url": f"{settings.FRONT_SITE}"}}],
+    [{"text": "🔖 Записаться на консультацию", "web_app": {"url": f"{settings.FRONT_SITE}"}}],
     [{"text": "ℹ️ О нас", "callback_data": "about_us"}]
 ]
 
 back_kb = [
     [{"text": "🏠 Главное меню", "callback_data": "home"}],
-    [{"text": "🔖 Записаться", "web_app": {"url": f"{settings.FRONT_SITE}"}}]
+    [{"text": "🔖 Записаться на консультацию", "web_app": {"url": f"{settings.FRONT_SITE}"}}]
 ]
 
 def generate_kb_profile(user_db_id: int, count_booking: int):
     kb_profile = [
         [{"text": "🏠 Главное меню", "callback_data": "home"}],
-        [{"text": "🔖 Записаться", "web_app": {"url": f"{settings.FRONT_SITE}"}}]
+        [{"text": "🔖 Записаться н консультацию", "web_app": {"url": f"{settings.FRONT_SITE}"}}]
     ]
     if count_booking > 0:
         kb_profile.append([{"text": f"🔒 Мои записи ({count_booking})", "callback_data": f"my_booking_{user_db_id}"}])
